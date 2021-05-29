460b1e41ceb71ce7e608c8e5637b3c99e361f6e4
VinySales <vinysalesreboucas@hotmail.com>
2021-05-29 17:27:49 -0300

    Criação da classe Board

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,7 +5,7 @@
  */
 package application;
 
-import boardgame.Position;
+import boardgame.Board;
 
 /**
  *
@@ -18,8 +18,8 @@ public class Program {
      */
     public static void main(String[] args) {
         
-        Position p = new Position(1, 2);
-        System.out.println(p);
+        Board b = new Board(8, 8);
+        
     }
     
 }
--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -10,5 +10,17 @@ package boardgame;
  * @author vinys
  */
 public class Piece {
+    protected Position position;
+    private Board board;
+
+    public Piece(Board board) {
+        this.board = board;
+        position = null;
+    }
+
+    protected Board getBoard() {
+        return board;
+    }
+    
     
 }
